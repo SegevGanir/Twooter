73187a0401c31e69fc097f711a251b12433ee80d
SegevGanir <Segev@ganir.net>
2023-02-17 00:34:36 +0000

    modified:   twooter/app.vue 	new file:   twooter/components/Logo/Tweeter.vue 	new file:   twooter/components/Sidebar/Left.vue

---
+++ twooter/components/Sidebar/Left.vue
@@ -0,0 +1,16 @@
+<template> 
+    <div>
+
+
+        <div class="p-2 my-2 bg-red-300 hover:rounded-full">
+            <nuxt-link to="/">
+                <div class="w-8 h-8">
+                    <LogoTweeter />
+                </div>
+            </nuxt-link>
+        </div>
+
+    </div>
+</template>
+<script setup>
+</script>
